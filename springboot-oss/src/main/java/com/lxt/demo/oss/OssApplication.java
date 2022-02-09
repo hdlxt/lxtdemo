ac6ae3e8a9d56d3d58d6f5e43eb675f0e39a4cb4
gitlablxt <839376636@qq.com>
2022-02-09 10:32:41 +0800

    save

--- springboot-oss/src/main/java/com/lxt/demo/oss/OssApplication.java
+++ springboot-oss/src/main/java/com/lxt/demo/oss/OssApplication.java
@@ -3,6 +3,8 @@ package com.lxt.demo.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class OssApplication {
 
