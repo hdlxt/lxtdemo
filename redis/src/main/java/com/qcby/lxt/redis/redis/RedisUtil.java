ffbdb7899efa4c97b6d33c5ad09eec19be18c534
gitlablxt <839376636@qq.com>
2022-02-26 15:44:47 +0800

    save

--- redis/src/main/java/com/qcby/lxt/redis/redis/RedisUtil.java
+++ redis/src/main/java/com/qcby/lxt/redis/redis/RedisUtil.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
  **/
 @Slf4j
 @Component
-public class RedisUtil {
+public class  RedisUtil {
 
     @Autowired
     private RedisTemplate redisTemplate;
--- redis/src/test/java/com/qcby/lxt/redis/TestRedis.java
+++ redis/src/test/java/com/qcby/lxt/redis/TestRedis.java
@@ -21,15 +21,35 @@ import java.util.List;
 @Slf4j
 public class TestRedis extends RedisApplicationTests{
 
-
-
-
     @Autowired
     private RedisUtil redisUtil;
 
     static List<ResourceVo> resourceVoList = new ArrayList<>();
     static UserVo userVo = null;
 
+    // 短信验证码的前缀
+    public final static  String REDIS_SMS_PREFIX = "sms_";
+
+
+    @Test
+    public void testEx() throws InterruptedException {
+        String code = "1234";
+        String tel = "1501111";
+        // 60s 超时
+        redisUtil.setex(REDIS_SMS_PREFIX+tel,code,60);
+        while(true){
+
+            Object r = redisUtil.get(REDIS_SMS_PREFIX+tel);
+            if(r != null){
+                log.info("短信验证码：{}",r.toString());
+            }else{
+                log.info("短信验证码不存在！");
+            }
+            // 每200ms打印一次
+            Thread.sleep(5000);
+        }
+
+    }
     static {
         // 数据初始化
         resourceVoList.add(ResourceVo.builder().id(1L).name("用户管理").url("/user").authStr("/user").build());
